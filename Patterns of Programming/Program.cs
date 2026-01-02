a4309cfc261aff08c7474805393f000a79c0cb5a
Iamfittz <vuitsyk.a@gmail.com>
2026-01-02 14:54:43 +0200

    L

--- Patterns of Programming/Program.cs
+++ Patterns of Programming/Program.cs
@@ -1,9 +1,18 @@
 ﻿using Patterns_of_Programming.SOLID;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Patterns_of_Programming
 {
     internal class Program
     {
+        public static void UseIt(Rectangle r)
+        {
+            int width = r.Width;
+            r.Height = 10;
+            //10*width
+            Console.WriteLine($"Expected area of {10*width}, " +
+                $"got {r.Area}");
+        }
         static void Main(string[] args)
         {
             #region SingleResponsibility
@@ -18,35 +27,45 @@ namespace Patterns_of_Programming
             #endregion
 
             #region Open-Closed Principle
-            
-            var apple = new Product("Apple", Color.Green, Size.Small);
-            var tree = new Product("Tree", Color.Green, Size.Large);
-            var house = new Product("House", Color.Blue, Size.Large);
 
-            Product[] products = { apple, tree, house };
+            //var apple = new Product("Apple", Color.Green, Size.Small);
+            //var tree = new Product("Tree", Color.Green, Size.Large);
+            //var house = new Product("House", Color.Blue, Size.Large);
+
+            //Product[] products = { apple, tree, house };
+            ////bad practice
+            //var pf = new ProductFilter();
+            //Console.WriteLine("Green products (bad practice):");
+
+            //foreach (var p in pf.FilterdByColor(products, Color.Green))
+            //    Console.WriteLine($" - {p.Name} is green");
+
+            //Console.ReadLine();
+            ////good Open-Closed architecture
+            //var bf = new BetterFilter();
+            //Console.WriteLine("Large products:");
+            //var largeSpec = new SizeSpecification(Size.Large);
+            //foreach (var p in bf.Filter(products,largeSpec))
+            //{
+            //    Console.WriteLine($" - {p.Name} is large");
+            //}
+
+            //var largeBlueSpec = new AndSpecification(largeSpec, new ColorSpecification(Color.Blue));
+
+            //foreach (var item in bf.Filter(products, largeBlueSpec))
+            //{
+            //    Console.WriteLine($" - {item.Name} is large and Blue");
+            //}
+            #endregion
+
+            #region Liskov Substitution Principle
+
+            var rc = new Rectangle(3, 2);
+            UseIt(rc);
+
             //bad practice
-            var pf = new ProductFilter();
-            Console.WriteLine("Green products (bad practice):");
-
-            foreach (var p in pf.FilterdByColor(products, Color.Green))
-                Console.WriteLine($" - {p.Name} is green");
-
-            Console.ReadLine();
-            //good Open-Closed architecture
-            var bf = new BetterFilter();
-            Console.WriteLine("Large products:");
-            var largeSpec = new SizeSpecifactaion(Size.Large);
-            foreach (var p in bf.Filter(products,largeSpec))
-            {
-                Console.WriteLine($" - {p.Name} is large");
-            }
-
-            var largeBlueSpec = new AndSpecification(largeSpec, new ColorSpecifactaion(Color.Blue));
-
-            foreach (var item in bf.Filter(products, largeBlueSpec))
-            {
-                Console.WriteLine($" - {item.Name} is large and Blue");
-            }
+            var sq = new Square(5);
+            UseIt(sq);
             #endregion
         }
     }
