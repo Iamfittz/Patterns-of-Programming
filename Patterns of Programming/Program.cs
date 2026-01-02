09a8e8e1b2a4d61a7d106a22fc3ca5d1416e92f4
Iamfittz <vuitsyk.a@gmail.com>
2026-01-02 12:51:19 +0200

    Single Responsibility

---
+++ Patterns of Programming/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Patterns_of_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal j = new Journal();
+            j.AddEntry("I'm happy today");
+            j.AddEntry("I'm ate a bug");
+            Console.WriteLine(j);
+
+            var p = new PersistanceManager();
+            var filename = @"C:\temp\temp.txt";
+            p.Save(j,filename);
+        }
+    }
+}
---
+++ Patterns of Programming/S.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns_of_Programming
+{
+    internal class S
+    {
+    }
+    public class Journal
+    {
+        private readonly List<string> entries
+            = new List<string>();
+
+        public void AddEntry(string text)
+        {
+            entries.Add(text);
+        }
+        public void RemoveEntry(int index)
+        {
+            if (index < entries.Count && index >= 0)
+            {
+                entries.RemoveAt(index);
+            }
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Load(string filename) { }
+        public void Load(Uri uri) { }
+    }
+
+    public class PersistanceManager
+    {
+        private void Verify(string filename) { }
+        public void Save(Journal journal, string filename, bool overwrite = false)
+        {
+            Verify(filename);
+            File.WriteAllText(filename, journal.ToString());
+        }
+    }
+
+}
